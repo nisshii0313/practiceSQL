38a1aa2ee8b1d6b17ef491030df73cf4f188b83a
Yuta Imanishi <yuta19990313@gmail.com>
2018-12-14 20:25:07 +0900

    Create 2.10.sql

---
+++ practice/2.10.sql
@@ -0,0 +1,5 @@
+/*Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.*/
+
+SELECT name, ROUND(gdp/population, -3)
+FROM world
+WHERE gdp > 1000000000000
