cc493e79d84c553db54c9213516ebe86768035e6
Yuta Imanishi <yuta19990313@gmail.com>
2018-12-18 18:33:45 +0900

    Create 4_6.sql

---
+++ SELECT_within_SELECT_Tutorial/4_6.sql
@@ -0,0 +1,7 @@
+/*Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)*/
+
+SELECT name
+  FROM world
+WHERE  gdp > ALL(SELECT gdp
+                 FROM world
+                 WHERE gdp>0 AND continent = 'Europe')
